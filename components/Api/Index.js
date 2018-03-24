50cde08ffcd4e71b552752bfe5d4ffabf16e59eb
ThotsaphonB <thotsaphonj@gmail.com>
2018-03-24 00:28:40 -0400

    updated Api Index.js

--- components/Api/Index.js
+++ components/Api/Index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+// import axios from 'axios';
 
 class Api extends Component {
   render() {
@@ -10,5 +11,30 @@ class Api extends Component {
     )
   }
 }
+
+// /*
+export default class personsList extends React.Component {
+	state = {
+		person: []
+	}
+
+	componentMount() {
+		axios.get('https://placeholder.com')
+			.then(res => {
+				const persons = res.data;
+				this.setState({ person });
+			})
+	}
+
+	render() {
+		return (
+			<ul>
+				{ this.state.persons.map(person => <li>{person.name}</li>)}
+			</ul>
+		)
+	}
+}
+
+// */
 
 export default Api
